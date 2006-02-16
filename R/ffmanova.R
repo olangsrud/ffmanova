01d89d0e4000027a7ab476bc46e9a034eaf3b9be
Bjørn-Helge Mevik <b-h@mevik.net>
2006-02-16 20:20:46 +0000

    First version of new formula interfacs

--- R/ffmanova.R
+++ R/ffmanova.R
@@ -1,31 +1,31 @@
 # %=============== ffmanova.m ====================
 # %  results = ffmanova(X,Y,cova,model,xNames,stand,nSim,Xnew)
 # %     or    results = ffmanova(modelFormula,stand,nSim,Xnew)
-# %    Performs general linear modelling of several response variables (Y). 
-# %    Collinear and highly correlated response variables are handled. 
-# %    The X-factors can be categorical, continuous and composite continuous 
+# %    Performs general linear modelling of several response variables (Y).
+# %    Collinear and highly correlated response variables are handled.
+# %    The X-factors can be categorical, continuous and composite continuous
 # %    (useful for experiments involving mixtures).
 # %
 # %     The function calculates
 # %     - 50-50 MANOVA results.
 # %     - raw single response p-values.
-# %     - familywise adjusted and false discovery rate adjusted single 
+# %     - familywise adjusted and false discovery rate adjusted single
 # %       response p-values by rotation testing.
 # %     - predictions, mean predictions and least squares means.
-# %     - standard deviations of those predictions. 
+# %     - standard deviations of those predictions.
 # %
 # % ==========   INPUT ==========:
-# %      X{1,#Xvariables} - design information as cell array. Categorical design variables 
-# %            can be represented by a numeric vector, a numeric matrix (each unique row 
-# %            is a group), a character matrix (each row representing a group name), or 
-# %            a cell array of strings stored as a column vector. Nonzero elements of 
-# %            cova indicate cells of X that are covariates. Multiple column of covariate 
-# %            model terms are allowed. 
+# %      X{1,#Xvariables} - design information as cell array. Categorical design variables
+# %            can be represented by a numeric vector, a numeric matrix (each unique row
+# %            is a group), a character matrix (each row representing a group name), or
+# %            a cell array of strings stored as a column vector. Nonzero elements of
+# %            cova indicate cells of X that are covariates. Multiple column of covariate
+# %            model terms are allowed.
 # %            - Alternatively X can be an ordinary matrix where each column
 # %            is a design variable.
-# %      Y(#observations,#responses) - matrix of response values. 
+# %      Y(#observations,#responses) - matrix of response values.
 # %              cova(1,#Xvariables) - covariate terms (see above)
-# %        model(#terms,#Xvariables) - model matrix or order coded model or 
+# %        model(#terms,#Xvariables) - model matrix or order coded model or
 # %                                    text coded model (see below)
 # %                           stand  - standardization of responses, = 0 (default) or 1
 # %                           xNames - Names of x factors. Default: {'A' 'B' 'C'}
@@ -33,16 +33,16 @@
 # %                            Xnew  - cell array of cell arrays Xnew = {Xnew1; Xnew2; ..},
 # %                                    new X's for prediction calculations.
 # %                            cXnew - cell array cXnew = {cXnew1(*,*); cXnew2(*,*); ..},
-# %                                    Predicts linear combinations (default: identity matrix) 
+# %                                    Predicts linear combinations (default: identity matrix)
 # %                                       cXnew*Xnew
 # %                          nSimXNew - When cXnew and nSimXNew are specified:
 # %                                       Significance tests according to cXnew*Xnew
-# %                                       50-50 MANOVA results 
+# %                                       50-50 MANOVA results
 # %                                       + rotation tests(when nSimXNew>0)
 # %
 # %   !!!! THE USE OF cXnew/nSimXNew is not implemented in this version !!!!!
 # %
-# %   NOTE:                                 
+# %   NOTE:
 # %       - Some cells of Xnew1 (and Xnew2...) can be empty ("[]") - leading
 # %              to mean predictions and least squares means.
 # %       - nSim can be a single number -> equal nSim for all terms.
@@ -58,14 +58,14 @@
 # %       - text coded model:
 # %              'linear'    is equivalent to { 1 1 1 ..... 1}
 # %              'quadratic' is equivalent to { 2 2 2 ..... 2}
-# %              'cubic'     is equivalent to { 3 3 3 ..... 3}                    
+# %              'cubic'     is equivalent to { 3 3 3 ..... 3}
 # %       - model matrix example: X = {A B C}
 # %                model = [1 0 0; 0 1 0 ; 0 0 1; 2 0 0; 1 1 0; 1 0 1; 0 1 1; 3 0 0]
 # %                 ->   Constant + A + B + C + A^2 + A*B + A*C + B*C + A^3
-# %           Constant term is automatically included. But create constant term 
+# %           Constant term is automatically included. But create constant term
 # %           manually ([0 0 0; ...]) to obtain constant term output.
 # %       - default model is the identity matrix -> main factor model
-# %                 
+# %
 # %         When X or Y is empty the model matrix is returned and printet (with matlab code)
 # %                examples: model = manova5050([],[],[0 1 0 1],{3 2 1 3});
 # %                          model = manova5050([],[],[0 1 0 1],'quadratic');
@@ -75,19 +75,19 @@
 # %  ::
 # %  ::    ffmanova('Y = A + B + A*B + C|D|E + F|G|H@2 + I|J|K#2 + L^3 + M#3 + N#4 - N^3')
 # %  ::        givs this model:   A    B    A*B    C    D    E    C*D  C*E  D*E  C*D*E
-# %  ::          F    G    H    F*G  F*H  G*H  
-# %  ::          I    J    K    I*J  I*K  J*K  I^2  J^2  K^2  
+# %  ::          F    G    H    F*G  F*H  G*H
+# %  ::          I    J    K    I*J  I*K  J*K  I^2  J^2  K^2
 # %  ::          L^3    M    M^2  M^3   N    N^2  N^4
 # %  ::
 # %  ::      @2 means interactions up to order 2
-# %  ::      #2 means all terms up to order 2 
-# %  :: 
-# %  ::      A variable is treated as categorical if $ is included at the end 
+# %  ::      #2 means all terms up to order 2
+# %  ::
+# %  ::      A variable is treated as categorical if $ is included at the end
 # %  ::      of the variable name (anywhere in a complex model formula).
 # %  ::      A variable that is cell array is treated as categorical (A->{A}).
 # %  ::
 # %  ::      Except that =,+,-,|,@,#,*,^ are special symbols in the model formula,
-# %  ::      ffmanova uses eval to interpret the string. 
+# %  ::      ffmanova uses eval to interpret the string.
 # %  ::      ffmanova('log(100+Y) = a + b==2 + 1./c')  is a valid expression.
 # %  ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # %  ::::
@@ -103,7 +103,7 @@
 # %       exVarBU: variance explained by (nPC+nBU) components
 # %       pValues: 50-50 MANOVA p-values.
 # %    outputText: 50-50 MANOVA results as text.
-# %          Yhat: Fitted values. 
+# %          Yhat: Fitted values.
 # %       YhatStd: Standard deviations of the fitted values.
 # %          nSim: as input (-1 -> 0), but could have been changed interactively.
 # %     pAdjusted: familywise adjusted p-values.
@@ -117,7 +117,7 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-# Xnew as input is not implemented in R 
+# Xnew as input is not implemented in R
 
 ffmanova = function(formel,Y,stand=1,nSim=0){
 model_data = modelData(formel)
@@ -131,6 +131,61 @@ res3 = unitests(xyObj)
 c(res1,res2,res3)
 }
 
+## Test of new formula interface:
+ffmanova2 <- function(formula, data, stand = TRUE, nSim = 0) {
+
+    ## META: We start with standard formula mumbo-jumbo...
+
+    ## Get the model frame.  META: This is unneccessary general for the
+    ## moment, but perhaps subset and na.action will be added later.
+    mf <- match.call(expand.dots = FALSE)
+    m <- match(c("formula", "data", "subset", "na.action"), names(mf), 0)
+    mf <- mf[c(1, m)]                # Retain only the named arguments
+    mf[[1]] <- as.name("model.frame")
+    mf <- eval(mf, parent.frame())
+
+    ## Get the terms
+    mt <- attr(mf, "terms")
+
+    ## Get the data matrices:
+    mm <- model.matrix(mt, mf)
+    Y <- model.response(mf, "numeric")
+    if (stand) Y <- stdStand(Y)
+
+    ## META: taken from modelData()
+    ## Create a `fator/term index matrix':
+    mOld = attr(mt, "factors")
+    ## Fix any I() terms:
+    mNew = fixModelMatrix(mOld)
+    ## add constant term
+    mNew = cbind("(Intercept)" = 0, mNew)
+    ## transpose
+    model = t(mNew)
+
+    ## META: taken from modelData()
+    ## Split the model matrix into matrices for each term:
+    termNr = attr(mm, "assign") + 1
+    D = vector("list", max(termNr))
+    for (i in seq(along = D))
+        D[[i]] <- mm[,termNr == i, drop = FALSE]
+
+    ## META: Hva gjør denne?  Den brukes bare her.
+    dObj <- d_Obj(D, model)
+
+    ## META: Hva gjør denne?  Den brukes bare her.
+    xyObj = xy_Obj(dObj, Y)
+
+    nTerms = length(xyObj$dObj$df_D_test)
+
+    ## Do the manova:
+    res1 = manova5050(xyObj,stand)
+    ## And the rotation tests:
+    res2 = rotationtests(xyObj, rep(nSim,length.out=nTerms))
+    ## And the univariate tests:
+    res3 = unitests(xyObj)
+    ## Return everything:
+    c(res1,res2,res3)
+}
 
 rotationtests = function(xyObj,nSim){
 nTerms = length(xyObj$dObj$df_D_test)
@@ -143,12 +198,12 @@ for(i in 1:nTerms){
    if(is.list(xyObj$errorObs)){
       res = rotationtest(xyObj$hypObs[[i]],xyObj$errorObs[[1]],nSim[i],xyObj$errorObs[[2]])
    }else{
-      res = rotationtest(xyObj$hypObs[[i]],xyObj$errorObs,nSim[i])  
+      res = rotationtest(xyObj$hypObs[[i]],xyObj$errorObs,nSim[i])
    } #end
-   pAdjusted[i,] = res$pAdjusted 
-   pAdjFDR[i,]   = res$pAdjFDR  
+   pAdjusted[i,] = res$pAdjusted
+   pAdjFDR[i,]   = res$pAdjFDR
    simN_ = c(simN_ ,res$simN)
-}   
+}
 list(pAdjusted=pAdjusted,pAdjFDR=pAdjFDR,simN=simN_)
 }
 
@@ -162,11 +217,11 @@ for(i in 1:nTerms){
    if(is.list(xyObj$errorObs)){
       res = uniTest(xyObj$hypObs[[i]],xyObj$errorObs[[1]],xyObj$errorObs[[2]])
    }else{
-      res = uniTest(xyObj$hypObs[[i]],xyObj$errorObs)  
+      res = uniTest(xyObj$hypObs[[i]],xyObj$errorObs)
    } #end
    pRaw[i,] = res$pValues
    stat[i,] = res$stat
-}   
+}
 list(pRaw=pRaw,stat=stat)
 }
 
