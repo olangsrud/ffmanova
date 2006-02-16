d3b962eb40391d528aceb7ff7fb55b32828d65f8
Bjørn-Helge Mevik <b-h@mevik.net>
2006-02-16 11:24:21 +0000

    Slight simplification in modelData()

--- R/ffmanova_div.R
+++ R/ffmanova_div.R
@@ -4,11 +4,11 @@ matlabColon <- function(from, to) { if(from > to) numeric(0) else from:to }
 # Author: Bjørn-Helge Mevik
 
 listX = function(X,f,...){
-df = c2df(X)   
-X = c2m(X)   
+df = c2df(X)
+X = c2m(X)
 output = f(X,...)
-output$X = m2c(output$X,df) 
-output   
+output$X = m2c(output$X,df)
+output
 }#end listX
 
 
@@ -18,33 +18,36 @@ norm = function(X){
 
 # modelData takes a model formula as input
 modelData = function(formel){
-mOld = attr(terms(formel), "factors")
-mNew = fixModelMatrix(mOld)
-# add constant term
-mNew = cbind(matrix(0,nrow(mNew),1),mNew)
-attr(mNew,"dimnames")[[2]][1]="(Intercept)"
-# transpose
-model = t(mNew)
-D = vector("list",nrow(model))
-mm =  model.matrix(formel)  
-termNr = attr(mm, "assign") + 1
-for(i in 1:ncol(mm)){
-  D[[termNr[i]]] = cbind(D[[termNr[i]]] , mm[,i,drop=FALSE])      
-}
-list(D=D,model=model)
+    ## Create a `term indices matrix':
+    mOld = attr(terms(formel), "factors")
+    mNew = fixModelMatrix(mOld)
+    ## add constant term
+    mNew = cbind("(Intercept)" = 0, mNew)
+    ## transpose
+    model = t(mNew)
+
+    ## Split the model matrix into matrices for each term:
+    D = vector("list",nrow(model))
+    mm =  model.matrix(formel)
+    termNr = attr(mm, "assign") + 1
+    for (i in seq(along = D))
+        D[[i]] <- mm[,termNr == i, drop = FALSE]
+
+    ## Return term matrices and indices matrix
+    list(D=D,model=model)
 }
 
 
 fixModelMatrix = function(mOld) {
-#print(mOld)    
+#print(mOld)
 mOld[mOld>0]=1 # Original m has "b=2" when  "b:I(a^2)"
-mNew = mOld  
+mNew = mOld
 varNamesOld = attr(mOld,'dimnames')[[1]]
 #varNamesNew = varNamesOld
 nVar = length(varNamesOld)
 nPower = rep(0,nVar)
 index = 1:nVar
-for(i in 1:nVar){  
+for(i in 1:nVar){
    lab = varNamesOld[i]
    lab = unlist(strsplit(lab, "I(", fixed = TRUE))
    if(length(lab)==2){
@@ -56,7 +59,7 @@ for(i in 1:nVar){
                if(length(lab)==1)
                   lab = c(lab,"1")
                if(length(lab)==2){
-                     if(lab[2] == sprintf('%d',(as.integer(lab[2])))){ # string-integer-test 
+                     if(lab[2] == sprintf('%d',(as.integer(lab[2])))){ # string-integer-test
                         for(j in 1:nVar){
                            if(lab[1] == varNamesOld[j]){
                                  nPower[i] = as.integer(lab[2])
@@ -64,19 +67,19 @@ for(i in 1:nVar){
                                  #varNamesNew[i] = labNew # Denne er ikke i bruk
                              }
                           }
-                       }   
-               }      
+                       }
+               }
           }
     }
- } 
-for(i in 1:nVar){  
+ }
+for(i in 1:nVar){
        if(index[i]!=i){
           a = rep(0,nVar)
           a[index[i]] = nPower[i]
           a = matrix(a,nVar,1)
           b = mOld[i,,drop=FALSE]
           mNew = mNew + a %*%b
-        }            
+        }
  }
  mNew = mNew[index==(1:nVar),,drop=FALSE]
 }# end  fixModelMatrix
@@ -86,13 +89,13 @@ for(i in 1:nVar){
 myrank = function(X,tol_ = 1e-9){ # Ny funksjon ikke matlab
 if(dim(X)[2]==0)                  # Kode hentet fra myorth
    return(X)
-S = svd(X,nv=0,nu=0)   
-S = S$d  
+S = svd(X,nv=0,nu=0)
+S = S$d
 r = length(S)
 tol = max(dim(X)) * S[1] * tol_
 if(!tol)
    return(0)
-while(S[r] < tol) 
+while(S[r] < tol)
    r=r-1
-r   
+r
 }# end myrank
