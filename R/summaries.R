ee999575006bb91e2d1585ba0eb90b9c47088921
Bjørn-Helge Mevik <b-h@mevik.net>
2006-02-17 17:14:14 +0000

    Implemented a print function

---
+++ R/summaries.R
@@ -0,0 +1,17 @@
+### summaries.R: print and summary functions
+### $Id$
+
+print.ffmanova <- function(x, digits = max(getOption("digits") - 3, 3), ...) {
+    cat("--- 50-50 MANOVA Version R1.0 ---",
+        sum(x$df), "(ok?) objects --",
+        ncol(x$pRaw), "(ok?) responses:\n")
+    tab <- with(x, data.frame(df, exVarSS, c(nPC, NA), c(nBU, NA),
+                              c(exVarPC, NA), c(exVarBU, NA), c(pValues, NA)))
+    dimnames(tab) <- list(c(bob$termNames, "Residuals"),
+                          c("Df", "exVarSS", "nPC", "nBU", "exVarPC",
+                            "exVarBU", "p-Value"))
+    tab <- tab[-1,]                     # Drop the (Intercept) row
+    printCoefmat(tab, digits = digits, cs.ind = 2, tst.ind = NULL,
+                 zap.ind = c(1,3,4), has.Pvalue = TRUE, na.print = "", ...)
+    invisible(tab)
+}
